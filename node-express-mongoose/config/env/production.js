68141d874baf99c68b63949423c1e44321943241
farmeter <farmeter@gmail.com>
2021-05-25 06:51:38 +0900

    mod env

--- node-express-mongoose/config/env/production.js
+++ node-express-mongoose/config/env/production.js
@@ -3,5 +3,5 @@
  */
 
 module.exports = {
-  db: process.env.MONGODB_URL || 'mongodb://localhost/my_app_production'
+  db: process.env.MONGODB_URL || 'mongodb://132.226.226.119:38017/my_app_production'
 };
